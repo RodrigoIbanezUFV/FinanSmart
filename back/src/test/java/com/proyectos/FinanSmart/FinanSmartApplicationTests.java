d9b6846440fd4f65795a6972962823b0f95e90cb
RodrigoIbanez <9106387@alumnos.ufv.es>
2025-02-15 14:14:15 +0100

    Commit inicial del proyecto con el contenido del proyecto con la estructura de carpetas

---
+++ back/src/test/java/com/proyectos/FinanSmart/FinanSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectos.FinanSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
